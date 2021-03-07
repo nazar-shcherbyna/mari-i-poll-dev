2d318a100cee2935ee17c32da9190eb0ea85813b
Nazar Shcherbyna <shherbina.nazar@gmail.com>
2021-03-07 14:37:07 +0200

    vue start

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+    <!-- <nav>
+        <div class="nav-wrapper container">
+        <a href="#" class="brand-logo"><img class="responsive-img" src="../assets/img/logo-real.png" alt="logo"></a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+            <li><a href="sass.html">Sass</a></li>
+            <li><a href="badges.html">Components</a></li>
+            <li><a href="collapsible.html">JavaScript</a></li>
+        </ul>
+        </div>
+    </nav> -->
+    <nav class="header__nav">
+        <div class="header__links">
+            <div class="header__logo">
+                <a href="#"><img src="img/logo-real.png" alt=""></a>
+            </div>
+            <ul>
+                <li><a class="header__link linksetting" href="#">Каталог</a></li>
+                <li><a class="header__link linksetting" href="#mip-about-us">О нас</a></li>
+                <li><a class="header__link linksetting" href="#contact">Контакты</a></li>
+            </ul>
+        </div>
+        <div class="header__contact">
+            <div class="header__tel"><a class="linksetting" href="tel:+380670000077">+38 068 391 18 05</a></div>
+            <div class="header__call"><a href="#myModal" data-hystmodal="#myModal" class="modal-open">Заказать звонок</a></div>
+        </div>
+    </nav>
+</template>
+
+<style lang="scss">
+
+</style>
